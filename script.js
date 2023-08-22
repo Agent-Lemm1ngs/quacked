abb9ed6acc957b41e63de328bf75fa7c4c8de9d5
Lucas Xie <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2023-08-22 21:43:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function addCookie() {
-  let num = document.querySelector("#count");
-  num.innerHTML = parseInt(num.innerHTML) + 1;
-  let bg = document.getElementById("bg");
-  const divElement = document.getElementById('bg');
-  const backgroundImage = getComputedStyle(divElement).backgroundImage;
-  const matches = backgroundImage.match(/(\d+)deg/);
-  const degree = matches ? parseInt(matches[1]) : null; 
-  divElement.style.background = `linear-gradient(${degree+2}deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)`;
+let loader;
+
+function loading() {
+  loader = setTimeout(showPage, 1000);
 }
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("loaded_done").style.display = "flex";
+}
+
+loading()
